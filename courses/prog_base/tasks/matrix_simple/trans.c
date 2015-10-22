862a226908c296f92ebb71fc7f37186bb4bbee1d
DariaDar <dariadar12@gmail.com>
2015-10-23 00:05:23 +0300

    matrix

---
+++ courses/prog_base/tasks/matrix_simple/trans.c
@@ -0,0 +1,104 @@
+void printMatrix (int mat [4][4])
+{
+    int i, j;
+
+    for (i = 0; i < 4; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			printf ("%i\t", mat [i][j]);
+		}
+		printf ("\n");
+	}
+	printf ("\n");
+}
+
+void fillRand(int mat[4][4])
+{
+	int i = 0;
+	int j = 0;
+
+	srand (time(NULL));
+	for (i = 0; i < 4; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			mat[i][j] = rand()%1999-999;
+		}
+
+	}
+
+}
+
+
+void transposMain(int mat[4][4])
+{
+	int i;
+	int j;
+	int mat1 [4][4];
+
+	for (i = 0; i < 4; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+		    mat1 [i][j] = mat [i][j];
+
+		}
+	}
+
+    for (i = 0; i < 4; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+
+            mat [i][j] = mat1[j][i];
+		}
+	}
+}
+
+void rotateCW180(int mat[4][4])
+{
+    int i,j;
+    int matCW [4][4];
+    int a,b;
+
+    for (i = 0, a = 3 ; i < 4; i++, a--)
+    {
+        for (j = 0, b = 3; j < 4; j++, b--)
+        {
+            matCW[a][b] = mat [i][j];
+        }
+    }
+
+    for (i = 0; i < 4; i++)
+        {
+            for (j = 0; j < 4; j++)
+            {
+                mat [i][j] = matCW[i][j];
+            }
+        }
+}
+
+
+void flipH(int mat[4][4])
+{
+    int i, j, k;
+    int mat3 [4][4];
+
+    for (i = 0; i < 4; i++)
+    {
+        for (j = 3, k = 0; j >= 0; j--, k++)
+        {
+            mat3[i][k] = mat [i][j];
+        }
+    }
+
+
+     for (i = 0; i < 4; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            mat[i][j] = mat3[i][j];
+        }
+    }
+}
