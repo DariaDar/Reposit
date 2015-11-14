ecf2c23aefd874e51350b7071c58a08e5e06ff5a
DariaDar <dariadar12@gmail.com>
2015-11-14 19:13:51 +0200

    cons

--- courses/prog_base/tasks/console/main.c
+++ courses/prog_base/tasks/console/main.c
@@ -2,70 +2,88 @@
 #include <stdlib.h>
 #include <Windows.h>
 
+#define colorXcnt  8
+#define colorYcnt 5
 
-int main(void) {
-    int x, y, k = 3, l = 1, s = 0, a = 0;
+ enum Colors
+    {
+        WHITE,
+        GREEN,
+        YELLOW
+    };
 
-
-    COORD position = {79, 25};
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
+void Draw(HANDLE hConsole, COORD position, WORD Color)
+{
     int col_yellow = BACKGROUND_INTENSITY| BACKGROUND_RED | BACKGROUND_GREEN;
     int col_green = BACKGROUND_INTENSITY|BACKGROUND_GREEN;
     int col_white = BACKGROUND_INTENSITY|BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE;
 
-
-
-    for(x = 79; x >= 0; x--)
+    switch(Color)
     {
-        for(y = 24; y >= 0; y--)
-        {
-            position.X = x;
-            position.Y = y;
-            SetConsoleCursorPosition(hConsole, position);
-
-
-                if((k % 3) == 2)
-                 SetConsoleTextAttribute(hConsole, col_yellow);
-                if((k % 3) == 1)
-                SetConsoleTextAttribute(hConsole, col_green);
-                if((k % 3) == 0)
-                SetConsoleTextAttribute(hConsole, col_white);
-
-
-
-            puts("*");
-            k = k+2;
-            Sleep(10);
-        }
-        s = s+2;
-        k = 3;
-        k = k+s;
-        x--;
-
-            for(y = 0; y <= 24; y++)
-            {
-                position.X = x;
-                position.Y = y;
-                SetConsoleCursorPosition(hConsole, position);
-
-                if((l % 3) == 1)
-                 SetConsoleTextAttribute(hConsole, col_white);
-                if((l % 3) == 2)
-                SetConsoleTextAttribute(hConsole, col_yellow);
-                if((l % 3) == 0)
-                SetConsoleTextAttribute(hConsole, col_green);
-
-                puts("*");
-                l = l+1;
-                Sleep(10);
-            }
-            l = 1;
-            a = a + 2;
-            l = l + a;
+    case WHITE:
+        SetConsoleTextAttribute(hConsole, col_white);
+        break;
+    case GREEN:
+        SetConsoleTextAttribute(hConsole, col_green);
+        break;
+    case YELLOW:
+        SetConsoleTextAttribute(hConsole, col_yellow);
+        break;
+    default:
+        break;
     }
 
+    SetConsoleCursorPosition(hConsole, position);
+    puts("*");
+    Sleep(10);
+}
 
-    return 0;
+int main(void)
+{
+    const int colorXsize = 10;
+    const int colorYsize = 5;
+    /*Матрица заданных цветов*/
+    int ColorMatrix[colorYcnt][colorXcnt] =
+    {
+        {YELLOW, WHITE, WHITE, YELLOW, GREEN, WHITE, YELLOW, GREEN},
+        {GREEN, YELLOW, WHITE, WHITE, YELLOW, GREEN, WHITE, YELLOW},
+        {WHITE, GREEN, YELLOW, WHITE, WHITE, YELLOW, GREEN, WHITE},
+        {YELLOW, WHITE, GREEN, YELLOW, WHITE, WHITE, YELLOW, GREEN},
+        {GREEN, YELLOW, WHITE, GREEN, YELLOW, WHITE, WHITE, YELLOW},
+    };
+
+    int i, j, k;
+    int x, y;
+
+    WORD curColor;
+    COORD position = {79, 25};
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    for(i = 79, k = 0; i >= 0;i--, k++)
+    {
+        position.X = i;
+        x = position.X/colorXsize;
+
+        if(k % 2 == 0)
+        {
+            for(j = 24; j >= 0; j--)
+            {
+                position.Y = j;
+                y = position.Y/colorYsize;
+                curColor = ColorMatrix[y][x];
+                Draw(hConsole,position,curColor);
+            }
+        }
+        else
+        {
+            for(j = 0; j <=24; j++)
+            {
+                position.Y = j;
+                y = position.Y/colorYsize;
+                curColor = ColorMatrix[y][x];
+                Draw(hConsole,position, curColor);
+            }
+        }
+    }
 }
 
