683d0af62e4a481ac2777d17ff768c888a133935
DariaDar <dariadar12@gmail.com>
2015-12-06 15:10:58 +0200

    dyn_mem

--- courses/prog_base/tasks/dyn_mem/main.c
+++ courses/prog_base/tasks/dyn_mem/main.c
@@ -4,25 +4,37 @@
 
 int main()
 {
-    int size, i, j, cnt = 0;
+    int * size = NULL;
+    int * i = NULL;
+    int * j = NULL;
+    int * cnt = NULL;
+
+    i = calloc(1, sizeof(int));
+    j = calloc(1, sizeof(int));
+    cnt = calloc(1, sizeof(int));
+    size = calloc(200, sizeof(int));
     char * str = calloc(200, sizeof(char));
     const char * exstraStr = "aeoiuyAEOIUY";
     printf("Enter your string: ");
     scanf("%s", str);
-    size = strlen(str)/sizeof(str[0]);
+    *size = strlen(str)/sizeof(str[0]);
 
-    for(i = 0; i < size; i++)
+    for(*i = 0; *i < *size; (*i)++)
     {
-        for(j = 0; j < 12; j++)
+        for(*j = 0; *j < 12; (*j)++)
         {
-            if(str[i] == exstraStr[j])
+            if(str[*i] == exstraStr[*j])
             {
-                cnt = cnt + 1;
+                *cnt = *cnt + 1;
             }
         }
     }
 
-    printf("Count of vowels: %i", cnt);
+    printf("Count of vowels: %i", *cnt);
     free(str);
+    free(i);
+    free(j);
+    free(cnt);
+    free(size);
     return 0;
 }
