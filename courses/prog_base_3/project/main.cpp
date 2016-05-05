9b4ed59824d5491774dec64bdb2648b6d40170b8
DariaDar <dariadar12@gmail.com>
2016-05-06 00:06:58 +0300

    time, control, animation

--- courses/prog_base_3/project/main.cpp
+++ courses/prog_base_3/project/main.cpp
@@ -1,8 +1,10 @@
-
  #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <math.h>
+#include <iostream>
  using namespace sf;
 
-
+ //For the first time. Simple realization of basic functions; not the main file
 class Player
  {
 public:
@@ -13,11 +15,9 @@ public:
 	Image image;
 	Texture texture;
 	Sprite sprite;
-	Player(String F, float X, float Y, float W, float H);
 
- };
 
-Player:: Player(String F, float X, float Y, float W, float H)
+	Player:: Player(String F, float X, float Y, float W, float H)
 {
 	dx = 0;
 	dy = 0;
@@ -37,26 +37,54 @@ Player:: Player(String F, float X, float Y, float W, float H)
 
 }
 
+	void control(float time)
+	{
+		static float currentFrame = 0;
+
+		if(Keyboard::isKeyPressed(Keyboard::Left))
+		{
+			currentFrame += 0.004 * time;
+			if(currentFrame > 3)
+				currentFrame -= 2;
+			sprite.setTextureRect(IntRect(295 * int(currentFrame), 260, 215,200));
+			sprite.move(-0.1 * time, 0);
+
+		}
+		if(Keyboard::isKeyPressed(Keyboard::Right))
+		{
+			currentFrame += 0.004 * time;
+			if(currentFrame > 3)
+				currentFrame -= 2;
+			sprite.setTextureRect(IntRect(290 * int(currentFrame), 490, 210, 200));
+			sprite.move(0.1 * time, 0);
+		}
+	}
+
+ };
+
+
 
 int main()
 {
 
 	RenderWindow window(sf::VideoMode(700, 700), "Window");
-	Clock clock;
 
-	Player p("cat_spritesheet_1.png",200, 270 ,210,200);
-	//p.sprite.setPosition(50,50);
+	Player p("cat_spritesheet_11.png",295, 260 ,210,200);
+	p.sprite.setPosition(50,50);
+
 	bool isMove = false;
 	float dX = 0, dY = 0;
 
+	Clock clock;
 
 	while (window.isOpen())
 	{
-
-		Vector2i pixelPos = Mouse::getPosition(window);
-		printf("%i\n", pixelPos.x);
-
 		sf::Event event;
+
+		float time = clock.getElapsedTime().asMicroseconds();
+		clock.restart();
+		time = time / 800;
+		Vector2i pixelPos = Mouse::getPosition(window);
 
 		while (window.pollEvent(event))
 		{
@@ -71,21 +99,26 @@ int main()
 							dY = pixelPos.y - p.sprite.getPosition().y;
 							isMove = true;
 						}
+
 			if (event.type == Event::MouseButtonReleased)
 				if (event.key.code == Mouse::Left)
 					isMove = false;
 					p.sprite.setColor(Color::White);
 		}
-
 		if (isMove)
 		{
 			p.sprite.setColor(Color::Green);
 			p.sprite.setPosition(pixelPos.x - dX,pixelPos.y - dY);
 		}
 
+
+
+		p.control(time); //for the first time
+
 		window.clear();
 		window.draw(p.sprite);
 		window.display();
 	}
+
 	return 0;
 }
